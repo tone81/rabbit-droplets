1f48e5e3d992dd8dc09f151d1b2d0956cff3aeac
Tony Nguyen <tony.nguyen.81@gmail.com>
2014-08-29 10:23:18 -0400

    don't work

--- test/first-droplet.js
+++ test/first-droplet.js
@@ -1,5 +1,14 @@
-var should = require('should'),
-    runPublisher = require('../lib/run-publisher.js'),
+var chai = require("chai"),
+    chaiAsPromised = require("chai-as-promised"),
+    proxyquire = require('proxyquire'),
+    when = require('when');
+
+chai.use(chaiAsPromised);
+
+var stub = {},
+    sut = proxyquire('../lib/run-publisher', { 'amqplib': stub });
+
+var runPublisher = require('../lib/run-publisher'),
     config = require('../config');
 
 describe("First set of unit tests", function() {
@@ -7,13 +16,10 @@ describe("First set of unit tests", function() {
     it('then it will return the queueName passed in', function() {
       // arrange
       var queueName = 'pellets';
-
-      // act
-      runPublisher(config.amqpUrl, queueName, 'doh').
-      then(function(actualQueueName) {
-        // assert
-        return actualQueueName.should.eventually.eql(queueName);
-      });
+      
+      // act & assert
+      return sut(config.amqpUrl, queueName, 'doh').
+        should.eventually.equal(queueName);
     });
 
     xit('then it will return the queueName and message passed in');
