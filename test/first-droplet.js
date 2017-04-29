3b3eca1460072c6fed16eadbe84d99c16b28843d
Recursive Lambda God <chris@circle.academy>
2017-04-29 13:49:28 -0400

    "Recursive Lambda Refactor V1"

--- test/first-droplet.js
+++ test/first-droplet.js
@@ -1,19 +1,17 @@
-var should = require('should'),
-    runPublisher = require('../lib/run-publisher.js'),
-    config = require('../config');
+var should = require('should');
+var runPublisher = require('../lib/run-publisher.js');
+var config = require('../config');
 
-describe("First set of unit tests", function() {
-  describe('when running the publisher', function() {
-    it('then it will return the queueName passed in', function() {
+describe("First set of unit tests", () => {
+  describe('when running the publisher', () => {
+    it('then it will return the queueName passed in', () => {
       // arrange
       var queueName = 'pellets';
 
       // act
       runPublisher(config.amqpUrl, queueName, 'doh').
-      then(function(actualQueueName) {
-        // assert
-        return actualQueueName.should.eventually.eql(queueName);
-      });
+      then(actualQueueName => // assert
+      actualQueueName.should.eventually.eql(queueName));
     });
 
     xit('then it will return the queueName and message passed in');
